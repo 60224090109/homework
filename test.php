c6d9e5beb65ce37e1bdf1253725579d297d6299b
60224090109 <54498061+60224090109@users.noreply.github.com>
2019-09-08 16:34:38 +0700

    Initial commit

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php 
+ 
+    
+    echo "<br>";
+    echo $_POST["name"]; 
+    echo "<br/>";
+    echo $_POST["gender"];
+    echo "<br/>";
+    echo $_POST["subject"]; 
+    echo "<br/>";
+    echo $_POST["province"];
+    echo "<br/>";
+    echo $_POST["mobile_number"];
+    echo "<br/>";
+    echo $_POST["address"];
+    echo "<br/>";
+
+?>
